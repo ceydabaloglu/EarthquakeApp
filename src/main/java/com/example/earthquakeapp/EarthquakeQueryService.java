acf5a4bb6c7001b622fe81e83586823f2b939727
ceydabaloglu <e170503036@stud.tau.edu.tr>
2022-12-12 23:32:40 +0300

    Listing is made according to country and time period.

--- src/main/java/com/example/earthquakeapp/EarthquakeQueryService.java
+++ src/main/java/com/example/earthquakeapp/EarthquakeQueryService.java
@@ -15,44 +15,26 @@ import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Locale;
 
 
 public class EarthquakeQueryService {
-    private URL Url ;
+
     public EarthquakeQueryService(){
 
     }
 
-    public List<Earthquake> findEarthquakes(){
-        List<Earthquake> elist = new ArrayList<Earthquake>();
-        return elist;
-    }
-
-    public String convertStreamToString(InputStream is) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        for (int ch; (ch = is.read()) != -1; ) {
-            sb.append((char) ch);
-        }
-        return sb.toString();
-    }
-
     //Try connection
     public String HttpResponseFromData (String days, String country){
-        String Response ="Turkei " ;
         StringBuilder resp = new StringBuilder();
-        try {
+        try{
             var date = LocalDate.now();
             var startDate = date.minusDays(Integer.parseInt(days));
             URL x = new URL("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + startDate.toString() +"&endtime=" + date.toString());
             HttpURLConnection conn = (HttpURLConnection) x.openConnection();
             conn.connect();
-            int responseCode = conn.getResponseCode();
             InputStream inputStream = conn.getInputStream();
-            StringBuilder output = new StringBuilder();
-
             if (inputStream != null) {
                 BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
                 StringBuilder responseStrBuilder = new StringBuilder();
@@ -60,19 +42,37 @@ public class EarthquakeQueryService {
                 String inputStr;
                 while ((inputStr = streamReader.readLine()) != null)
                     responseStrBuilder.append(inputStr);
-                String jsonString = responseStrBuilder.toString() ; //assign your JSON String here
+                String jsonString = responseStrBuilder.toString(); //assign your JSON String here
                 JSONObject obj = new JSONObject(jsonString);
                 JSONArray features = (JSONArray) obj.get("features");
                 for(int i = 0 ; i < features.length(); i++){
                     JSONObject earthquake = (JSONObject)features.get(i);
+                    JSONObject geometry = (JSONObject)earthquake.get("geometry");
                     JSONObject properties = (JSONObject)earthquake.get("properties");
                     var place = properties.get("place");
-                    var mag = properties.get("mag");
-                    var timeEarthquake = properties.get("time");
-                    Date happenedDate = new Date((long)timeEarthquake);
-                    DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-                    String formatted = format.format(happenedDate);
-                    resp.append("Place " + place.toString() + " Magnitude " +  mag.toString() + " Date/Time " + formatted + "<br>" );
+                    String countrySubbed = place.toString().substring(place.toString().lastIndexOf(",") + 2, place.toString().length());
+                    if(isCountry(countrySubbed)){
+                        String EarthQuakeCountry = countrySubbed;
+                        if(EarthQuakeCountry.compareTo(country) == 0 ){
+                            var mag = properties.get("mag");
+                            var timeEarthquake = properties.get("time");
+                            Date happenedDate = new Date((long)timeEarthquake);
+                            DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                            String formatted = format.format(happenedDate);
+                            resp.append("Country "+ EarthQuakeCountry + " Place " + place.toString() + " Magnitude " +  mag.toString() + " Date/Time " + formatted + "<br>" );
+                        }
+                    }
+                    else{
+                        //This means this is a state of USA.
+                        if(country.compareTo("United States") == 0 && place != null){
+                            var mag = properties.get("mag");
+                            var timeEarthquake = properties.get("time");
+                            Date happenedDate = new Date((long)timeEarthquake);
+                            DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                            String formatted = format.format(happenedDate);
+                            resp.append("Country "+ "United States" + " Place " + place.toString() + " Magnitude " +  mag.toString() + " Date/Time " + formatted + "<br>" );
+                        }
+                    }
                 }
             }
 
@@ -85,5 +85,18 @@ public class EarthquakeQueryService {
             e.printStackTrace();
         }
         return resp.toString();
+    }
+
+    public boolean isCountry(String country){
+        String[] countryCodes = Locale.getISOCountries();
+        for(String countryCode : countryCodes){
+            Locale locale = new Locale("",countryCode);
+            String code = locale.getCountry();
+            String name = locale.getDisplayCountry();
+            if(country.compareTo(name) == 0){
+                return true;
+            }
+        }
+        return false;
     }
 }
